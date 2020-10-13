887de7a18466a77a9d3604701dea2bb51b9868e4
Hemant Raj <62658209+hemant767@users.noreply.github.com>
2020-10-14 01:13:04 +0530

    Authentication Done.

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/UserDB" , {useNewUrlParser:true , useUnifiedTopology:true});
+
+const UserSchema = {
+    firstName:String,
+    lastName:String,
+    email:String,
+    password:String
+    
+};
+
+const User =  mongoose.model("User",UserSchema);
 
 app.get("/login",function(req,res){
     res.sendFile(__dirname + "/login.html");
@@ -16,24 +29,62 @@ app.get("/forgot",function(req,res){
     res.sendFile(__dirname + "/forgot-password.html")
 });
 
-
 app.get("/sign_up",function(req,res){
     res.sendFile(__dirname + "/Sign-Up.html")
 });
 
 app.post("/sign_up",function(req,res){
 
-    const f_name = req.body.f_name;
-    const l_name = req.body.l_name;
-    const email_up = req.body.email_up;
-    const pass_up = req.body.pass_up;
-    const cnpass_up = req.body.cnpass_up;
+    if(req.body.pass_up === req.body.cnpass_up){
+
+    
+        const newUser = new User({
+            firstName:req.body.f_name,
+            lastName:req.body.l_name,
+            email:req.body.email_up,
+            password:req.body.pass_up,
+            
+        })
+
+        User.findOne({email:req.body.email_up},function(err,foundUser){
+            if(err){
+                console.log(err);
+            }else{
+                if(foundUser){
+                    console.log("not saved");
+                    res.redirect("/sign_up");
+                }else{
+                    newUser.save()
+                }
+            }
+        })
+                   
+    }
+    else{
+        console.log("not saved");
+    }
+    
 });
 
 app.post("/login",function(req,res){
     
     const email_1 = req.body.Email_1;
     const pass = req.body.Password_1;
+
+    User.findOne({email:email_1},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password === pass){
+                    res.sendFile(__dirname + "/Homepage.html")
+                }else{
+                    res.redirect("/login");
+                }
+            }
+        }
+    })
+
 });
 
 app.post("/forgot",function(req,res){
@@ -46,7 +97,7 @@ app.post("/forgot",function(req,res){
     if (new_pass !== re_new_pass){
         res.redirect("/forgot");
     }else{
-        res.redirect("/login");
+        res.sendFile(__dirname + "/HomePage.html");
     }   
 });
 
